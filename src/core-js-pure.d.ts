c80a462eacc0fc9bf5b529d362ba6125fc6dbbe3
Alex Mendes <alexander.mendes@immediate.co.uk>
2020-03-06 14:54:24 +0000

    Add polyfill for starts with

--- src/core-js-pure.d.ts
+++ src/core-js-pure.d.ts
@@ -2,3 +2,8 @@ declare module 'core-js-pure/features/object/from-entries' {
   const fromEntries: typeof Object.fromEntries
   export default fromEntries
 }
+
+declare module 'core-js-pure/features/string/virtual/starts-with' {
+  const startsWith: typeof String.prototype.startsWith
+  export default startsWith
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ export interface RestProps {
 }
 
 import fromEntries from 'core-js-pure/features/object/from-entries';
+import startsWith from 'core-js-pure/features/string/virtual/starts-with';
 
 const dataProps = (props: RestProps): RestProps =>
   fromEntries(
     Object.entries(props).filter(([name, value]) => {
       // don't include non-data attributes
-      if (!name.startsWith('data-')) {
+      if (!startsWith.call(name, 'data-')) {
         return false;
       }
 
